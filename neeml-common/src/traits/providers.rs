01cac19d0476a4800c65043bc5c93ed5b8b64203
Dom Rodriguez <shymega@shymega.org.uk>
2024-04-30 00:24:48 +0100

    refactor: Reorganise features, fix `cfg` usage, update lockfile

--- neeml-common/src/traits/providers.rs
+++ neeml-common/src/traits/providers.rs
@@ -1,12 +1,18 @@
 /* 'Remotes' providers */
 
 pub trait BaseRemoteProvider {
-    fn connect(&self) -> Result<(), String>;
-    fn is_connected(&self) -> bool;
+    fn connect(&self) -> Result<(), String> {
+        Err(String::from("Not supported.")) /* Maildirs/Mbox don't work this way, for example */
+    }
+    fn is_connected(&self) -> bool {
+        false
+    }
     fn is_disconnected(&self) -> bool {
         !self.is_connected()
     }
-    fn disconnect(&self) -> Result<(), String>;
+    fn disconnect(&self) -> Result<(), String> {
+        Err(String::from("Not supported.")) /* Maildirs/Mbox don't work this way, for example */
+    }
 }
 
 #[cfg(feature = "providers-mail")]
@@ -18,33 +24,45 @@ pub mod mail {
 
     #[cfg(feature = "providers-mail")]
     pub trait MailRemoteProvider: BaseRemoteProvider {
-        fn get_credentials(&self) -> Option<HashMap<String, String>>;
-        fn authenticate(&self, credentials: HashMap<String, String>) -> Result<(), String>;
-        fn authenticate_with_oauth2(&self, token: &str) -> Result<(), String>;
-        fn refresh_token(&self) -> Result<(), String>;
-        fn authenticate_anonymous(&self) -> Result<(), String>;
-        fn fetch(&self) -> Result<(), String>;
+        fn get_credentials(&self) -> Option<HashMap<String, String>> {
+            None /* Maildirs/Mbox don't need authentication, for example */
+        }
+        fn authenticate(&self, _credentials: HashMap<String, String>) -> Result<(), String> {
+            Err(String::from("Not supported.")) /* Maildirs/Mbox don't need authentication, for example */
+        }
+        fn authenticate_with_oauth2(&self, _token: &str) -> Result<(), String> {
+            Err(String::from("Not supported.")) /* Maildirs/Mbox don't need authentication, for example */
+        }
+        fn refresh_token(&self) -> Result<(), String> {
+            Err(String::from("Not supported.")) /* Maildirs/Mbox don't need authentication, for example */
+        }
+        fn authenticate_anonymous(&self) -> Result<(), String> {
+            Err(String::from("Not supported.")) /* Maildirs/Mbox don't need authentication, for example */
+        }
+        fn fetch(&self) -> Result<(), String> {
+            Ok(())
+        }
         fn refresh(&self) -> Result<(), String> {
             self.fetch()
         }
     }
 
-    #[cfg(feature = "providers-imap")]
+    #[cfg(feature = "providers-mail-imap")]
     pub trait ImapRemoteProvider: MailRemoteProvider {
         fn get_imap_server(&self) -> Option<String>;
     }
 
-    #[cfg(feature = "providers-jmap")]
+    #[cfg(feature = "providers-mail-jmap")]
     pub trait JmapRemoteProvider: MailRemoteProvider {
         fn get_jmap_server(&self) -> Option<String>;
     }
 
-    #[cfg(feature = "providers-maildir")]
+    #[cfg(feature = "providers-mail-maildir")]
     pub trait MaildirRemoteProvider: MailRemoteProvider {
         fn get_maildir_path(&self) -> Option<PathBuf>;
     }
 
-    #[cfg(feature = "providers-mbox")]
+    #[cfg(feature = "providers-mail-mbox")]
     pub trait MboxRemoteProvider: MailRemoteProvider {
         fn get_mbox_path(&self) -> Option<PathBuf>;
     }
@@ -54,102 +72,97 @@ pub mod mail {
         fn send_mail(&self, mail: &str) -> Result<(), String>;
     }
 
-    #[cfg(feature = "providers-sendmail")]
+    #[cfg(feature = "providers-mail-sendmail")]
     pub trait SendmailOutgoingMailRemoteProvider: OutgoingMailRemoteProvider {}
 
-    #[cfg(feature = "providers-smtp")]
+    #[cfg(feature = "providers-mail-smtp")]
     pub trait SmtpOutgoingMailRemoteProvider: OutgoingMailRemoteProvider {}
 
-    #[cfg(feature = "providers-exchange")]
+    #[cfg(feature = "providers-mail-exchange")]
     pub trait ExchangeMailRemoteProvider: MailRemoteProvider {
         fn get_exchange_server(&self) -> Option<String>;
         fn get_ews_url(&self) -> Option<String>;
     }
 }
 
-#[cfg(feature = "providers-contexts")]
-pub mod contexts {
-    use super::BaseRemoteProvider;
-
-    pub trait ContextsRemoteProvider: BaseRemoteProvider {
-        fn fetch(&self) -> Result<(), String>;
-        fn refresh(&self) -> Result<(), String> {
-            self.fetch()
-        }
-    }
-
-    #[cfg(feature = "providers-contexts-exchange")]
-    pub trait ExchangeContextsRemoteProvider: ContextsRemoteProvider {
-        fn get_exchange_server(&self) -> Option<String>;
-        fn get_ews_url(&self) -> Option<String>;
-    }
-
-    #[cfg(feature = "providers-contexts-gmail")]
-    pub trait GmailContextsRemoteProvider: ContextsRemoteProvider {
-        fn get_gmail_server(&self) -> Option<String>;
-    }
-
-    #[cfg(feature = "providers-contexts-notmuch")]
-    pub trait NotmuchContextsRemoteProvider: ContextsRemoteProvider {
-        fn get_notmuch_path(&self) -> Option<String>;
-    }
-
-    #[cfg(feature = "providers-contexts-dav")]
-    pub trait DavRemoteProvider: ContextsRemoteProvider {
-        fn get_dav_url(&self) -> Option<String>;
-    }
-
-    #[cfg(all(
-        feature = "providers-contexts-caldav",
-        feature = "providers-contexts-dav"
-    ))]
-    pub trait CaldavRemoteProvider: DavRemoteProvider {
-        fn get_caldav_url(&self) -> Option<String>;
-    }
-
-    #[cfg(all(
-        feature = "providers-contexts-carddav",
-        feature = "providers-contexts-dav"
-    ))]
-    pub trait CarddavRemoteProvider: DavRemoteProvider {
-        fn get_carddav_url(&self) -> Option<String>;
-    }
-}
-
+#[cfg(feature = "providers-completion")]
 pub mod completion {
-    use super::contexts::ContextsRemoteProvider;
     use super::BaseRemoteProvider;
+    pub trait CompletionRemoteProvider: BaseRemoteProvider {}
 
-    pub trait CompletionRemoteProvider: BaseRemoteProvider {
-        fn fetch(&self) -> Result<(), String>;
-        fn refresh(&self) -> Result<(), String> {
-            self.fetch()
+    #[cfg(feature = "providers-contexts")]
+    pub mod contexts {
+        use super::BaseRemoteProvider;
+
+        #[cfg(feature = "providers-contexts")]
+        pub trait ContextsRemoteProvider: BaseRemoteProvider {
+            fn fetch(&self) -> Result<(), String>;
+            fn refresh(&self) -> Result<(), String> {
+                self.fetch()
+            }
+        }
+
+        #[cfg(feature = "providers-contexts-exchange")]
+        pub trait ExchangeContextsRemoteProvider: ContextsRemoteProvider {
+            fn get_exchange_server(&self) -> Option<String>;
+            fn get_ews_url(&self) -> Option<String>;
+        }
+
+        #[cfg(feature = "providers-contexts-gmail")]
+        pub trait GmailContextsRemoteProvider: ContextsRemoteProvider {
+            fn get_gmail_server(&self) -> Option<String>;
+        }
+
+        #[cfg(feature = "providers-contexts-notmuch")]
+        pub trait NotmuchContextsRemoteProvider: ContextsRemoteProvider {
+            fn get_notmuch_path(&self) -> Option<String>;
+        }
+
+        #[cfg(feature = "providers-contexts-dav")]
+        pub trait DavRemoteProvider: ContextsRemoteProvider {
+            fn get_dav_url(&self) -> Option<String>;
+        }
+
+        #[cfg(all(
+            feature = "providers-contexts-caldav",
+            feature = "providers-contexts-dav"
+        ))]
+        pub trait CaldavRemoteProvider: DavRemoteProvider {
+            fn get_caldav_url(&self) -> Option<String>;
+        }
+
+        #[cfg(all(
+            feature = "providers-contexts-carddav",
+            feature = "providers-contexts-dav"
+        ))]
+        pub trait CarddavRemoteProvider: DavRemoteProvider {
+            fn get_carddav_url(&self) -> Option<String>;
         }
     }
 
-    pub trait ContextsCompletionRemoteProvider:
-        CompletionRemoteProvider + ContextsRemoteProvider
-    {
-        fn get_contexts(&self) -> Result<(), String>;
+    #[cfg(feature = "providers-completion-llm")]
+    pub mod llm {
+        use crate::traits::providers::completion::CompletionRemoteProvider;
+
+        #[cfg(feature = "providers-completion-llm")]
+        pub trait LlmCompletionRemoteProvider: CompletionRemoteProvider {
+            fn get_llm_url(&self) -> Option<String>;
+            fn query_llm_prompt(&self) -> Result<String, String>;
+        }
+
+        #[cfg(feature = "providers-completion-llm-openai")]
+        pub trait OpenAiCompletionRemoteProvider: LlmCompletionRemoteProvider {}
+
+        #[cfg(feature = "providers-completion-llm-ollama")]
+        pub trait OllamaCompletionRemoteProvider: LlmCompletionRemoteProvider {}
     }
-
-    #[cfg(feature = "providers-contexts-llm")]
-    pub trait LlmCompletionRemoteProvider: CompletionRemoteProvider {
-        fn get_llm_url(&self) -> Option<String>;
-        fn query_llm_prompt(&self) -> Result<String, String>;
-    }
-
-    #[cfg(feature = "providers-contexts-openai")]
-    pub trait OpenAiCompletionRemoteProvider: LlmCompletionRemoteProvider {}
-
-    #[cfg(feature = "providers-contexts-ollama")]
-    pub trait OllamaCompletionRemoteProvider: LlmCompletionRemoteProvider {}
 }
 
 #[cfg(feature = "providers-news")]
 pub mod news {
     use super::BaseRemoteProvider;
 
+    #[cfg(feature = "providers-news")]
     pub trait NewsRemoteProvider: BaseRemoteProvider {
         fn fetch(&self) -> Result<(), String>;
         fn refresh(&self) -> Result<(), String> {
@@ -157,13 +170,13 @@ pub mod news {
         }
     }
 
-    #[cfg(feature = "providers-nntp")]
+    #[cfg(feature = "providers-news-nntp")]
     pub trait NntpRemoteProvider: NewsRemoteProvider {
         fn configure(&self);
         fn send_article(&self, article: &str) -> Result<(), String>;
     }
 
-    #[cfg(feature = "providers-rss")]
+    #[cfg(feature = "providers-news-rss")]
     pub trait RssRemoteProvider: NewsRemoteProvider {
         fn get_rss_url(&self) -> Option<String>;
         fn parse_rss(&self) -> Result<(), String>;
@@ -173,6 +186,8 @@ pub mod news {
 #[cfg(feature = "providers-storage")]
 pub mod storage {
     use super::BaseRemoteProvider;
+
+    #[cfg(feature = "providers-storage")]
     pub trait StorageRemoteProvider: BaseRemoteProvider {
         fn fetch(&self) -> Result<(), String>;
         fn refresh(&self) -> Result<(), String> {
@@ -181,13 +196,13 @@ pub mod storage {
         fn sync(&self) -> Result<(), String>;
     }
 
-    #[cfg(feature = "providers-rsync")]
+    #[cfg(feature = "providers-storage-rsync")]
     pub trait RsyncRemoteProvider: StorageRemoteProvider {
         fn snapshot(&self) -> Result<(), String>;
         fn get_rsync_path(&self) -> Option<String>;
     }
 
-    #[cfg(feature = "providers-sqlite")]
+    #[cfg(feature = "providers-storage-sqlite")]
     pub trait SqliteRemoteProvider: StorageRemoteProvider {
         fn query(&self, query: &str) -> Result<(), String>;
         fn get_db_path(&self) -> Option<String>;
